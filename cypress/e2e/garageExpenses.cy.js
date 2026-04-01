3024159ed19dfcec560affa2ee4aaeb2203a7f14
Yuliia Hladii <yu.hladii@gmail.com>
2026-04-01 18:00:19 +0300

    Homework22.1

--- cypress/e2e/garageExpenses.cy.js
+++ cypress/e2e/garageExpenses.cy.js
@@ -1,37 +1,101 @@
 import GaragePage from "../support/pages/GaragePage";
-import ExpensePage from "../support/pages/ExpensePage";
-
-describe("Garage + Expense", () => {
-  it("should add car and fuel expense", () => {
-    cy.login(Cypress.env("email"), Cypress.env("password"));
-
-    //add car
+ 
+describe("Garage + API + Expenses", () => {
+  let carId;
+ 
+  const createCarViaUi = () => {
+    cy.intercept("POST", "/api/cars").as("createCar");
+ 
     GaragePage.addCarButton().click();
-    GaragePage.addCarBrand()
-      .find("option:selected")
-      .should("contain.text", "Audi");
-    GaragePage.addCarModel()
-      .find("option:selected")
-      .should("contain.text", "TT");
+ 
+    GaragePage.addCarBrand().select("Audi");
+    GaragePage.addCarModel().select("TT");
     GaragePage.addCarMileage().type("101");
-    GaragePage.addButton().should("be.enabled");
-    cy.get("form").should("have.class", "ng-valid");
-    cy.get(".invalid-feedback").should("not.exist");
+ 
     GaragePage.addButton().click();
-    cy.get("ngb-modal-window").should("not.be.visible");
-
-    cy.contains("Audi TT").should("be.visible");
-
-    //add expense
-    cy.contains(".car-item", "Audi TT").within(() => {
-      cy.contains("button", "Add fuel expense").click();
+ 
+    return cy.wait("@createCar").then((interception) => {
+      expect(interception.response.statusCode).to.eq(201);
+ 
+      carId = interception.response.body.data.id;
+      return carId;
     });
+  };
+ 
+  beforeEach(() => {
+    cy.login(Cypress.env("email"), Cypress.env("password"));
+  });
+ 
 
-    ExpensePage.addExpenseMileage().type("101");
-    ExpensePage.addExpenseLiters().type("20");
-    ExpensePage.addExpenseTotalCost().type("1000");
-    ExpensePage.addButton().click();
+  // 1. CREATE CAR (UI + INTERCEPT)
 
-    cy.contains("20").should("be.visible");
+  it("Create car (UI + intercept)", () => {
+    createCarViaUi();
+ 
+    cy.contains("Audi TT").should("be.visible");
+  });
+ 
+
+  // 2. VERIFY CAR (API)
+
+  it("Verify car via API", () => {
+    createCarViaUi().then((createdCarId) => {
+      cy.request({
+        method: "GET",
+        url: "/api/cars",
+      }).then((resp) => {
+        expect(resp.status).to.eq(200);
+ 
+        const car = resp.body.data.find((c) => c.id === createdCarId);
+ 
+        expect(car).to.exist;
+        expect(car.brand).to.eq("Audi");
+        expect(car.model).to.eq("TT");
+      });
+    });
+  });
+ 
+ 
+  // 3. CREATE EXPENSE (API + UI CHECK)
+
+ it("Create expense via API + check UI", () => {
+    createCarViaUi().then((createdCarId) => {
+      const mileage = 200;
+      const liters = 20;
+      const totalCost = 1000;
+      const reportedAt = "2026-04-01T00:00:00.000Z";
+ 
+      cy.createExpense(createdCarId, mileage, liters, totalCost, reportedAt).then((resp) => {
+        expect(resp.status).to.eq(200);
+        expect(resp.body.data.carId).to.eq(createdCarId);
+        expect(resp.body.data.mileage).to.eq(mileage);
+        expect(resp.body.data.liters).to.eq(liters);
+        expect(resp.body.data.totalCost).to.eq(totalCost);
+ 
+        const createdExpenseId = resp.body.data.id;
+ 
+        cy.request({
+          method: "GET",
+          url: "/api/expenses",
+        }).then((expensesResp) => {
+          expect(expensesResp.status).to.eq(200);
+ 
+          const createdExpense = expensesResp.body.data.find(
+            (expense) => expense.id === createdExpenseId
+          );
+ 
+          expect(createdExpense).to.exist;
+          expect(createdExpense.carId).to.eq(createdCarId);
+          expect(createdExpense.liters).to.eq(liters);
+          expect(createdExpense.totalCost).to.eq(totalCost);
+        });
+      });
+ 
+      cy.contains(".car-item", "Audi TT", { timeout: 10000 }).should("be.visible");
+ 
+      cy.contains(".car-item", "Audi TT").within(() => {
+        cy.contains("button", "Add fuel expense").should("be.visible").click();
+      });
+    });
   });
 });
